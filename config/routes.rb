caec30bbf304791ece65b1ea44dc6d503f354e50
Annie Yang <annie.ghostie@gmail.com>
2015-03-02 22:57:27 -0500

    fix user_path

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'main#index'
 
-  resources :lists, :restaurants
+  resources :users, :lists, :restaurants
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
