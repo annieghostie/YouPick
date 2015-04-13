3c562ba31a6972a261235274e34bf35a4dccc705
Annie Yang <annie.ghostie@gmail.com>
2015-04-12 22:54:09 -0400

    add yelp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404203640) do
+ActiveRecord::Schema.define(version: 20150404204537) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150404203640) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
